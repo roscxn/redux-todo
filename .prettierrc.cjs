a47a76c3bf683a905b4d9cb60fb82e2661d737e5
Roslin <roscxn@gmail.com>
2024-05-13 23:04:18 +0800

    Formatting

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+// https://prettier.io/docs/en/configuration.html
+
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: false,
+    singleQuote: false,
+  };
+  
+  module.exports = config;
