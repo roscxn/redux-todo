d714a58829a4d4b7e50229f47604126eefbc46c6
Roslin <roscxn@gmail.com>
2024-05-20 08:59:36 +0800

    Commit test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:prettier/recommended",
-    "plugin:diff/diff"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
