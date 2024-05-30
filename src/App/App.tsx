a98230abc619e97fa8fa1ed95c756b215861a053
Roslin <roscxn@gmail.com>
2024-05-30 10:15:54 +0800

    test

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import { ThemeProvider } from "@mui/material"
-import { theme } from "../store/theme/theme"
+import { theme } from "../theme"
 
 import BottomNav from "../components/BottomNav/BottomNav"
 import MenuAppBar from "../components/MenuAppBar/MenuAppBar"
